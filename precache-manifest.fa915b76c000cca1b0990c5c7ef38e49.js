ed68be0ecc2197bf53eb9c72ff1afec9cd91b047
Mgin <mavangin@gmail.com>
2020-10-15 19:14:55 -0700

    Updates

---
+++ precache-manifest.fa915b76c000cca1b0990c5c7ef38e49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ce877f404e5c5cc882360dad7f7ff8c",
+    "url": "/weatherApp/index.html"
+  },
+  {
+    "revision": "1b48e9c9694a8d3c55c6",
+    "url": "/weatherApp/static/css/main.0dafb7e8.chunk.css"
+  },
+  {
+    "revision": "562705fa24b50ca4d4e7",
+    "url": "/weatherApp/static/js/2.a0530719.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatherApp/static/js/2.a0530719.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b48e9c9694a8d3c55c6",
+    "url": "/weatherApp/static/js/main.6659b5a2.chunk.js"
+  },
+  {
+    "revision": "efba474b3fd5a4a7aa5f",
+    "url": "/weatherApp/static/js/runtime-main.d4919a9c.js"
+  }
+]);
